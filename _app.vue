c31f94ad587c7e969ca86161203f0ed843ad82ba
Matt Strayer <git@mattstrayer.com>
2024-03-14 12:27:55 -0400

    added sunset theme :)

--- _app.vue
+++ _app.vue
@@ -1,8 +1,23 @@
+<script setup lang="ts">
+useHead({
+  title: "mindful",
+  htmlAttrs: {
+    lang: "en",
+    dir: "ltr"
+  }
+})
+
+// For Nuxt 3
+definePageMeta({
+  colorMode: "sunset"
+})
+</script>
+
 <template>
   <div class="container flex flex-col justify-center max-w-[600px] mx-auto">
     <h1
       class="text-zinc-100 text-6xl text-center font-extrabold mb-4 line leading-snug">
-      be<br>mindful
+      be<br />mindful
     </h1>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+useHead({
+  title: "mindful",
+  htmlAttrs: {
+    lang: "en",
+    dir: "ltr"
+  }
+})
+
+definePageMeta({
+  colorMode: "sunset"
+})
+</script>
+
+<template>
+  <div class="container flex flex-col justify-center max-w-[600px] mx-auto">
+    <h1
+      class="text-zinc-100 text-6xl text-center font-extrabold mb-4 line leading-snug">
+      be<br />mindful
+    </h1>
+
+    <button class="btn btn-primary">Button</button>
+    <button class="btn btn-secondary">Button</button>
+  </div>
+</template>
