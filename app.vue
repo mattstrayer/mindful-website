e58ec82960317b7be1bde9fc50a033bc5f2a2bd8
Matt Strayer <git@mattstrayer.com>
2024-03-13 20:21:44 -0400

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="p-4">
-    <button class="btn">Hello daisyUI</button>
+  <div class="container flex flex-col justify-center max-w-[600px] mx-auto">
+    <h1
+      class="text-zinc-100 text-6xl text-center font-extrabold mb-4 line leading-snug">
+      be<br>mindful
+    </h1>
   </div>
 </template>
