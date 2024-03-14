64812fc1451b77e3fe34a28e3ce339cb6f7ce0e7
Matt Strayer <git@mattstrayer.com>
2024-03-14 19:23:35 -0400

    header

--- pages/index.vue
+++ pages/index.vue
@@ -13,22 +13,22 @@ definePageMeta({
 </script>
 
 <template>
-  <div class="container flex flex-col justify-center max-w-[600px] mx-auto">
-    <h1
-      class="text-zinc-100 text-6xl text-center font-extrabold mb-4 line leading-snug">
-      mindful
-    </h1>
-
-    <h3 class="prose">coming soon</h3>
-    <div class="mockup-browser border bg-base-300">
-      <div class="mockup-browser-toolbar">
-        <div class="input" />
-      </div>
-      <div class="flex justify-center bg-base-200">
-        <img src="/img/extension_preview.png" alt="mindful-extension-preview" />
+  <div>
+    <Header />
+    <div class="container flex flex-col justify-center max-w-[600px] mx-auto">
+      <h1 class="">coming soon</h1>
+      <div class="mockup-browser border bg-base-300">
+        <div class="mockup-browser-toolbar">
+          <div class="input" />
+        </div>
+        <div class="flex justify-center bg-base-200">
+          <img
+            src="/img/extension_preview.png"
+            alt="mindful-extension-preview" />
+        </div>
       </div>
     </div>
-  </div>
 
-  <Footer />
+    <Footer />
+  </div>
 </template>
