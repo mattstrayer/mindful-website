07699af11ca21b33f1911c7f91ec9ba97e45b83f
Matt Strayer <git@mattstrayer.com>
2024-04-13 12:17:48 -0400

    incomplete landing page
    but good enough to publish

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,10 @@ definePageMeta({
 <template>
   <div>
     <Header />
-    <div class="container flex flex-col justify-center max-w-[600px] mx-auto">
+    <div class="container flex flex-col justify-center max-w-[600px] mx-auto h-screen">
+      <h1 class="text-zinc-100 text-6xl text-center font-extrabold mb-4 line leading-snug p-12">
+        coming soon
+      </h1>
       <div class="mockup-browser border bg-base-300">
         <div class="mockup-browser-toolbar">
           <div class="input" />
