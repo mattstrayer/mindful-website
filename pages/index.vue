d761e004d89734cd1750e5163e6e4d3c5e566411
Matt Strayer <git@mattstrayer.com>
2024-04-16 20:23:38 -0400

    first cut of text outline

--- pages/index.vue
+++ pages/index.vue
@@ -47,6 +47,47 @@ useSeoMeta({
         </div>
       </div>
     </div>
+    <div class="container flex flex-col justify-center max-w-[600px] mx-auto h-screen">
+      <h1 class="text-zinc-100 text-6xl text-center font-extrabold mb-4 line leading-snug p-12">
+        stay focused on your goals
+      </h1>
+      <p class="text-zinc-100 text-center text-4xl">plan your day out. just a few simple, achievable, goals that you want to accomplish this day. </p>
+    </div>
+    <div class="container flex flex-col justify-center max-w-[600px] mx-auto h-screen">
+      <h1 class="text-zinc-100 text-6xl text-center font-extrabold mb-4 line leading-snug p-12">
+        new tab, same goals
+      </h1>
+      <p class="text-zinc-100 text-center text-4xl">mindful appears in every new tab you open. There to remind you of what you want to get done today</p>
+    </div>
+    <div class="container flex flex-col justify-center max-w-[600px] mx-auto h-screen">
+      <h1 class="text-zinc-100 text-6xl text-center font-extrabold mb-4 line leading-snug p-12">
+        block distractions with a click
+      </h1>
+      <p class="text-zinc-100 text-center text-4xl">with the mindful shield up, you'll be redirected to the mindful page when you try to access a distracting website</p>
+      <p class="text-zinc-100 text-center">don't worry -- if you disable the shield, mindful will restore any tabs that it had blocked</p>
+    </div>
+    <div class="container flex flex-col justify-center max-w-[600px] mx-auto h-screen">
+      <h1 class="text-zinc-100 text-6xl text-center font-extrabold mb-4 line leading-snug p-12">
+        set intentions
+      </h1>
+      <p class="text-zinc-100 text-center text-4xl">intentions help you set adjectives about yourself that you wish to be. You'll see them on every mindful page fading in and out.</p>
+
+    </div>
+    <div class="container flex flex-col justify-center max-w-[600px] mx-auto h-screen">
+      <h1 class="text-zinc-100 text-6xl text-center font-extrabold mb-4 line leading-snug p-12">
+        have a zen moment
+      </h1>
+      <p class="text-zinc-100 text-center text-4xl">speaking of intentions fading in and out. the animation for intentions doubles as a relaxation breathing excercise.
+        as a new intention shows, breathe in.
+        once it is fully visible, hold your breathe.
+        breathe out as it fades out.
+
+      </p>
+      <p class="text-zinc-100 text-center">
+        This follows the 4-7-8 breathing pattern, which has been proven to be calming and reduce anxiety.
+      </p>
+
+    </div>
 
     <Footer />
   </div>
