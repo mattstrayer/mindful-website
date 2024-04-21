979d1b2dc80828a2022ba28cd65d4e517b2f5b7d
Matt Strayer <git@mattstrayer.com>
2024-04-21 19:54:59 -0400

    add `inmind` theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 module.exports = {
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   darkMode: "class",
+
   daisyui: {
-    themes: ["sunset"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "sunset", // name of one of the included themes for dark mode
+    themes: [
+      {
+        inmind: {
+          primary: "#3B4883",
+          secondary: "#FF7124",
+          accent: "#DBBBA7",
+          neutral: "#202124",
+          "base-100": "#272D4E",
+          info: "#84a5d6",
+          success: "#2E4e2e",
+          warning: "#7e762e",
+          error: "#4e2e2e",
+        },
+      },
+    ], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "inmind", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
